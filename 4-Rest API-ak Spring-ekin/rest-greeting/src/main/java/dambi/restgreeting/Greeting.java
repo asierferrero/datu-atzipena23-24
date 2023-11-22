2d9b406da7e0f747d5cd9a3098856337372dbed8
asierferrero <ferrero.asier@uni.eus>
2023-11-22 12:28:09 +0100

    rest-greeting done

--- 4-Rest API-ak Spring-ekin/rest-greeting/src/main/java/dambi/restgreeting/Greeting.java
+++ 4-Rest API-ak Spring-ekin/rest-greeting/src/main/java/dambi/restgreeting/Greeting.java
@@ -1,3 +1,3 @@
 package dambi.restgreeting;
 
-public record Greeting(long id, String content) {}
+public record Greeting(long id, String content) { }
--- 4-Rest API-ak Spring-ekin/rest-greeting/src/main/java/dambi/restgreeting/GreetingController.java
+++ 4-Rest API-ak Spring-ekin/rest-greeting/src/main/java/dambi/restgreeting/GreetingController.java
@@ -6,28 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
+    private String template = "Hello, %s!"; // Default value
     private final AtomicLong counter = new AtomicLong();
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+            @RequestParam(value = "hizkuntza", defaultValue = "Hello") String hizkuntza) {
+        if (hizkuntza.equals("eus")) {
+            template = "Kaixo, %s!";
+            if (name.equals("World")) {
+                name = "Mundua";
+            }
+        } else if (hizkuntza.equals("es")) {
+            template = "Hola, %s!";
+            if (name.equals("World")) {
+                name = "Mundo";
+            }
+        } else if (hizkuntza.equals("eng")) {
+            if (name.equals("World")) {
+                name = "World";
+            }
+        }
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    @GetMapping("/hello")
-    public String greeting2(@RequestParam(value = "name", defaultValue = "World") String name) throws JsonProcessingException {
-        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
-
-        // Jackson erabiliz JSON dokumentu bihurtu
-        String json = objectMapper.writeValueAsString(greeting);
-
-        // Json dokumentua itzuli
-        return json;
     }
 }
