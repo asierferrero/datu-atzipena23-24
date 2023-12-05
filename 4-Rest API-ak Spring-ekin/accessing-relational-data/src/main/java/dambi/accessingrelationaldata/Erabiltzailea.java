99fb8317cdcd2ab4aa201e70b7c9af9f225a4f2e
asierferrero <ferrero.asier@uni.eus>
2023-12-05 14:18:26 +0100

    accessing relational data finished

--- 4-Rest API-ak Spring-ekin/accessing-relational-data/src/main/java/dambi/accessingrelationaldata/Erabiltzailea.java
+++ 4-Rest API-ak Spring-ekin/accessing-relational-data/src/main/java/dambi/accessingrelationaldata/Erabiltzailea.java
@@ -10,14 +10,32 @@ public class Erabiltzailea {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
-    public String izena;
 
-    public String getIzena() {
-        return izena;
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setIzena(String izena) {
-        this.izena = izena;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- 4-Rest API-ak Spring-ekin/accessing-relational-data/src/main/java/dambi/accessingrelationaldata/ErabiltzaileaController.java
+++ 4-Rest API-ak Spring-ekin/accessing-relational-data/src/main/java/dambi/accessingrelationaldata/ErabiltzaileaController.java
@@ -1,31 +1,63 @@
 package dambi.accessingrelationaldata;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/hasi")
+@RestController
+@RequestMapping(path = "/demo")
 public class ErabiltzaileaController {
     @Autowired
     private ErabiltzaileaRepository erabiltzaileaRepository;
 
-    @PostMapping(path="/gehitu")
-    public @ResponseBody String erabiltzaileaGehitu(@RequestParam String izena) {
+    @PostMapping(path = "/add")
+    public @ResponseBody String erabiltzaileaGehitu(@RequestParam int id, @RequestParam String name, @RequestParam String email) {
         Erabiltzailea erabiltzailea = new Erabiltzailea();
-        erabiltzailea.setIzena(izena);
+        erabiltzailea.setId(id);
+        erabiltzailea.setName(name);
+        erabiltzailea.setEmail(email);
         erabiltzaileaRepository.save(erabiltzailea);
         return "Erabiltzilea ondo gorde da";
     }
 
-    @GetMapping(path = "/denak")
+    @GetMapping(path = "/all")
     public @ResponseBody Iterable<Erabiltzailea> erabiltzaileGuztiak() {
         // This returns a JSON or XML with the users
         return erabiltzaileaRepository.findAll();
     }
 
+    @PutMapping(value = "/update/{userId}")
+    public ResponseEntity updateContact(@RequestBody Erabiltzailea erabiltzailea, @PathVariable int userId) {
+        try {
+            erabiltzailea.setId(userId);
+            erabiltzaileaRepository.save(erabiltzailea);
+
+            return ResponseEntity.ok().build();
+
+        } catch (Exception ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(path = "/delete/{userId}")
+    public ResponseEntity deleteUserById(@PathVariable int userId) {
+        try {
+            erabiltzaileaRepository.deleteById(userId);
+            return ResponseEntity.ok().build();
+
+        } catch (Exception ex) {
+            System.out.println("Errorea " + userId + " userra ezabatzerakoan. ");
+            return ResponseEntity.notFound().build();
+
+        }
+    }
 }
